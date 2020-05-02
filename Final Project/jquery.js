45ac44b08df21f03e2f0ecf464b6ee83bd5108a4
Sofia Cacho Sousa <sofiacsv@gmail.com>
2020-05-01 20:56:31 -0500

    s

--- Final Project/jquery.js
+++ Final Project/jquery.js
@@ -1,40 +1,5 @@
-$(document).ready(function(){
-});
+var audio1 = document.getElementById("audioID");
 
-$(document).ready(function(){
-});
-
-$("#submitmsg").click(function(){	
-    var clientmsg = $("#usermsg").val();
-    $.post("post.php", {text: clientmsg});				
-    $("#usermsg").attr("value", "");
-    return false;
-});
-
-function loadLog(){		
-
-    $.ajax({
-        url: "log.html",
-        cache: false,
-        success: function(html){		
-            $("#chatbox").html(html); //Insert chat log into the #chatbox div				
-          },
-    });
-}
-
-function loadLog(){		
-    var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
-    $.ajax({
-        url: "log.html",
-        cache: false,
-        success: function(html){		
-            $("#chatbox").html(html); //Insert chat log into the #chatbox div	
-            
-            //Auto-scroll			
-            var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
-            if(newscrollHeight > oldscrollHeight){
-                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-            }				
-          },
-    });
-}
+function playAudio() {
+    audio1.play();
+  }
