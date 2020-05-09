98e5715550af7cd3b269d9f0daa5a4468390c7cf
Sofia Cacho Sousa <sofiacsv@gmail.com>
2020-05-08 19:38:07 -0500

    v

--- Final Project/script.js
+++ Final Project/script.js
@@ -12,3 +12,18 @@ var liveVideoImage = document.querySelector(".live_video");
 liveVideoImage.addEventListener("click", function() {
     audioBox.play();
 })
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("live_video");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 600);
+}
