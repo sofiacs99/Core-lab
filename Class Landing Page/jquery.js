a33b9e899bdb19c6edf5edc6b830d388a2903396
Sofia Cacho Sousa <sofiacsv@gmail.com>
2020-05-09 02:03:29 -0500

    b

--- Class Landing Page/jquery.js
+++ Class Landing Page/jquery.js
@@ -8,16 +8,16 @@
     }
 
     var myIndex = 0;
-    carousel();
+        carousel();
 
-    function carousel() {
+        function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+        x[i].style.display = "none";  
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 3000);    
+    setTimeout(carousel, 2000);
     }
